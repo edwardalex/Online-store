7469e1c524fbf97046bfd767d935722703cd6718
Edward Alex <51661100+edwardalex@users.noreply.github.com>
2023-03-18 17:54:23 +0300

    Create components for the product

---
+++ src/Components/ListingPage/ListingPageDetails.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { ternaryCheck, setCurrencySign } from "../../Utils/Helpers";
+import Context from "../../Utils/Context";
+import "./ListingPageDetails.css";
+
+export class ListingPageDetails extends Component {
+  static contextType = Context;
+  render() {
+    const { currency } = this.context;
+    const { brand, name, prices, inStock } = this.props.product;
+    const currencySign = [currency[0]];
+    const itemPrice = prices[currency[1]].amount;
+
+    return (
+      <div className={ternaryCheck(inStock, "listing-page-details", "listing-page-details-out")}>
+        <p className="listing-page-name">
+          {brand} {name}
+        </p>
+
+        <p className="listing-page-price">
+          {setCurrencySign(currencySign)}
+          {itemPrice}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default ListingPageDetails;
