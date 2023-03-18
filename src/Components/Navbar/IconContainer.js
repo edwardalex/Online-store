32d7697742c5d0d3ee867cb0a39d3bb115d6c9ea
Edward Alex <51661100+edwardalex@users.noreply.github.com>
2023-03-18 17:36:57 +0300

    Create components for the product

---
+++ src/Components/Navbar/IconContainer.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { ternaryCheck, setCurrencySign, itemCounter } from "../../Utils/Helpers";
+import ArrowUp from "../../Assets/arrowDown.svg";
+import ArrowDown from "../../Assets/arrowUp.svg";
+import Cart from "../../Assets/cart.svg";
+import Context from "../../Utils/Context";
+import "./IconContainer.css";
+
+export class IconContainer extends Component {
+  static contextType = Context;
+  render() {
+    const { currency, addedItems } = this.context;
+    const { isMinicartOpen, isCurrenciesOpen, currenciesOpen, currenciesRef, minicartRef } = this.props;
+
+    const currencySign = [currency[0]];
+
+    return (
+      <div className="icon-container" ref={this.currencyIcon}>
+        <div className="currency-icon-container" onClick={() => isCurrenciesOpen(true)} ref={currenciesRef}>
+          {setCurrencySign(currencySign)}
+          <img src={ternaryCheck(currenciesOpen, ArrowDown, ArrowUp)} alt={"arrow"} />
+        </div>
+        <div className="cart-icon-container" onClick={() => isMinicartOpen(true)} ref={minicartRef}>
+          <img src={Cart} alt="cart" className="cart-icon" />
+          {addedItems.length > 0 && <div className="cart-item-counter">{itemCounter(addedItems)}</div>}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default IconContainer;
