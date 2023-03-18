b72bb38cbb3bbb07d46d2de075f019b7deb32300
Edward Alex <51661100+edwardalex@users.noreply.github.com>
2023-03-18 18:41:59 +0300

    Create components for the product

---
+++ src/Components/CartPage/CartProductCounter.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { increaseCounter, decreaseCounter } from "../../Utils/Helpers";
+import Minus from "../../Assets/minus.svg";
+import Plus from "../../Assets/plus.svg";
+import Context from "../../Utils/Context";
+import "./CartProductCounter.css";
+
+export class CartProductCounter extends Component {
+  static contextType = Context;
+  render() {
+    const { item, itemIndex } = this.props;
+    const { setCounter, removeItem } = this.context;
+    const counter = item[item.length - 2];
+
+    return (
+      <div className="counter-container">
+        <img className="cart-counter-button" src={Plus} alt="plus-box" onClick={() => increaseCounter(counter, itemIndex, setCounter)} />
+        <div className="cart-product-counter">{counter}</div>
+        <img
+          className="cart-counter-button"
+          src={Minus}
+          alt="minus-box"
+          onClick={() => {
+            decreaseCounter(counter, itemIndex, setCounter, removeItem);
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default CartProductCounter;
---
+++ src/Components/CartPage/CartProductDetails.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { ternaryCheck, setCurrencySign, isChecked, isAttributeColor } from "../../Utils/Helpers";
+import Context from "../../Utils/Context";
+import "./CartProductDetails.css";
+
+export class CartProductDetails extends Component {
+  static contextType = Context;
+  render() {
+    const { name, brand, prices, attributes, itemIndex } = this.props;
+    const { currency, switchAttributes, addedItems } = this.context;
+    const currencySign = [currency[0]];
+    const itemPrice = prices[currency[1]].amount;
+
+    return (
+      <div className="cart-product-details">
+        <p className="cart-product-brand">{brand}</p>
+        <p className="cart-product-name">{name}</p>
+        <p className="cart-product-price">
+          {setCurrencySign(currencySign)}
+          {itemPrice}
+        </p>
+
+        {attributes.map((attribute, attributeIndex) => {
+          const attributeKind = attribute.name === "Color";
+          return (
+            <React.Fragment key={`${attribute.type}:${attributeIndex}`}>
+              <p className="cart-attribute-name">{attribute.name}:</p>
+              <div className="cart-product-attributes">
+                {attribute.items.map((attributeItem) => (
+                  <React.Fragment key={`${attribute.name}:${attributeItem.value}`}>
+                    <input
+                      checked={isChecked(attributeIndex, attributeItem, addedItems, itemIndex)}
+                      type="radio"
+                      name={`${name}:${attribute.name}:${itemIndex}`}
+                      value={attributeItem.value}
+                      id={`${name}:${attribute.name}:${attributeItem.value}:${itemIndex}`}
+                      onChange={() => {
+                        switchAttributes(itemIndex, attributeIndex, attributeItem);
+                      }}
+                    />
+                    <label
+                      htmlFor={`${name}:${attribute.name}:${attributeItem.value}:${itemIndex}`}
+                      className={ternaryCheck(attributeKind, "color-label", "normal-label")}
+                      style={{
+                        backgroundColor: `${isAttributeColor(attributeKind, attributeItem)}`,
+                      }}
+                    >
+                      {ternaryCheck(attributeKind, "", attributeItem.value)}
+                    </label>
+                  </React.Fragment>
+                ))}
+              </div>
+            </React.Fragment>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default CartProductDetails;
