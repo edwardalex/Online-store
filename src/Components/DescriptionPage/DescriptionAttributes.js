a47bf644f1577a98c3c6fd176a9d9d93a59f2176
Edward Alex <51661100+edwardalex@users.noreply.github.com>
2023-03-18 18:17:48 +0300

    Create components for the product

---
+++ src/Components/DescriptionPage/DescriptionAttributes.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Attribute from "./Attribute";
+import "./DescriptionAttributes.css";
+
+export class DescriptionAttributes extends Component {
+  render() {
+    const { selected, setSelected, attributes, name } = this.props;
+
+    return (
+      <>
+        {attributes.map((attribute, attributeIndex) => {
+          const attributeKind = attribute.name === "Color";
+          return (
+            <React.Fragment key={`${name}:${attribute.name}`}>
+              <p className="attribute-name">{attribute.name}:</p>
+              <div className="product-attributes">
+                {attribute.items.map((item) => {
+                  return (
+                    <Attribute
+                      key={`${name}:${item.value}`}
+                      attributeKind={attributeKind}
+                      attributeIndex={attributeIndex}
+                      selected={selected}
+                      setSelected={setSelected}
+                      attribute={attribute}
+                      name={name}
+                      item={item}
+                    />
+                  );
+                })}
+              </div>
+            </React.Fragment>
+          );
+        })}
+      </>
+    );
+  }
+}
+
+export default DescriptionAttributes;
---
+++ src/Components/DescriptionPage/DescriptionDetails.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import parse from "html-react-parser";
+import update from "react-addons-update";
+import { ternaryCheck, setCurrencySign } from "../../Utils/Helpers";
+import DescriptionAttributes from "./DescriptionAttributes";
+import Context from "../../Utils/Context";
+import "./DescriptionDetails.css";
+
+export class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: [],
+    };
+
+    this.defaultAttributes = this.defaultAttributes.bind(this);
+    this.setSelected = this.setSelected.bind(this);
+  }
+
+  setSelected = (attribute, attributeIndex, item) => {
+    this.setState(
+      update(this.state, {
+        selected: {
+          [attributeIndex + 1]: {
+            $set: [attribute.name, item.value],
+          },
+        },
+      })
+    );
+  };
+
+  defaultAttributes = (id, attributes) => {
+    let selected = [id];
+    attributes.map((attribute) => selected.push([attribute.name, attribute.items[0].value]));
+    selected.push(1);
+    selected.push(this.props.prices.map((price) => price.amount));
+    this.setState({
+      selected: selected,
+    });
+  };
+
+  componentDidMount() {
+    const { id, attributes } = this.props;
+    this.defaultAttributes(id, attributes);
+  }
+
+  static contextType = Context;
+
+  render() {
+    const { id, name, brand, prices, attributes, inStock, description } = this.props;
+    const { currency, addItem } = this.context;
+    const { selected } = this.state;
+    const currencySign = [currency[0]];
+    const itemPrice = prices[currency[1]].amount;
+
+    return (
+      <div className="product-details">
+        <p className="product-brand">{brand}</p>
+        <p className="product-name">{name}</p>
+        <form>
+          <DescriptionAttributes id={id} attributes={attributes} name={name} selected={selected} setSelected={this.setSelected} />
+          <p className="product-price-title">PRICE:</p>
+          <p className="product-price-amount">
+            {setCurrencySign(currencySign)}
+            {itemPrice}
+          </p>
+          <button
+            disabled={ternaryCheck(inStock, false, true)}
+            className={ternaryCheck(inStock, "add-button", "disabled-button")}
+            onClick={(event) => {
+              event.preventDefault();
+              addItem(selected, event);
+            }}
+          >
+            {ternaryCheck(inStock, "ADD TO CART", "OUT OF STOCK")}
+          </button>
+        </form>
+        <div className="product-description">{parse(description)}</div>
+      </div>
+    );
+  }
+}
+
+export default Details;
