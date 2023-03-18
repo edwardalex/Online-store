eca5a24a3f3847734589ed5e2378e597e789b9b8
Edward Alex <51661100+edwardalex@users.noreply.github.com>
2023-03-18 17:42:38 +0300

    Create components for the product

---
+++ src/Components/Minicart/MinicartProductCounter.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { increaseCounter, decreaseCounter } from "../../Utils/Helpers";
+import miniPlus from "../../Assets/miniPlus.svg";
+import miniMinus from "../../Assets/miniMinus.svg";
+import Context from "../../Utils/Context";
+import "./MinicartProductCounter.css";
+
+export class MinicartProductCounter extends Component {
+  static contextType = Context;
+  render() {
+    const { item, itemIndex } = this.props;
+    const { setCounter, removeItem } = this.context;
+    const counter = item[item.length - 2];
+
+    return (
+      <div className="minicart-counter-container">
+        <img className="minicart-counter-button" src={miniPlus} alt="plus-box" onClick={() => increaseCounter(counter, itemIndex, setCounter)} />
+        <div className="minicart-product-counter">{counter}</div>
+        <img
+          className="minicart-counter-button"
+          src={miniMinus}
+          alt="minus-box"
+          onClick={() => {
+            decreaseCounter(counter, itemIndex, setCounter, removeItem);
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default MinicartProductCounter;
---
+++ src/Components/Minicart/MinicartProductDetails.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { ternaryCheck, setCurrencySign, isChecked, isAttributeColor } from "../../Utils/Helpers";
+import Context from "../../Utils/Context";
+import "./MinicartProductDetails.css";
+
+export class MinicartDetails extends Component {
+  static contextType = Context;
+  render() {
+    const { name, brand, prices, attributes, itemIndex } = this.props;
+    const { currency, switchAttributes, addedItems } = this.context;
+    const currencySign = [currency[0]];
+    const itemPrice = prices[currency[1]].amount;
+
+    return (
+      <div className="minicart-product-details">
+        <p>{brand}</p>
+        <p>{name}</p>
+        <p className="minicart-price">
+          {setCurrencySign(currencySign)}
+          {itemPrice}
+        </p>
+
+        {attributes.map((attribute, attributeIndex) => {
+          const attributeKind = attribute.name === "Color";
+          return (
+            <React.Fragment key={`${attribute.type}:${attributeIndex}`}>
+              <p className="minicart-attribute-name">{attribute.name}:</p>
+              <div className="minicart-product-attributes">
+                {attribute.items.map((attributeItem) => (
+                  <React.Fragment key={`${attribute.name}:${attributeItem.value}`}>
+                    <input
+                      checked={isChecked(attributeIndex, attributeItem, addedItems, itemIndex)}
+                      type="radio"
+                      name={`minicart:${name}:${attribute.name}:${itemIndex}`}
+                      value={attributeItem.value}
+                      id={`minicart:${name}:${attribute.name}:${attributeItem.value}:${itemIndex}`}
+                      onChange={() => {
+                        switchAttributes(itemIndex, attributeIndex, attributeItem);
+                      }}
+                    />
+                    <label
+                      htmlFor={`minicart:${name}:${attribute.name}:${attributeItem.value}:${itemIndex}`}
+                      className={ternaryCheck(attributeKind, "color-label", "normal-label")}
+                      style={{
+                        backgroundColor: `${isAttributeColor(attributeKind, attributeItem)}`,
+                      }}
+                    >
+                      {ternaryCheck(attributeKind, "", attributeItem.value)}
+                    </label>
+                  </React.Fragment>
+                ))}
+              </div>
+            </React.Fragment>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default MinicartDetails;
